f195a1c9c31c319c1b929012603a663e8c8275d3
wyz <wang_yuzhong@126.com>
2018-04-30 17:02:41 +0800

    upd city/wyz/std

---
+++ problems/city/wyz/std.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const int N=3003;
+
+const int mod=59393;
+int cost[N];
+int dp[N],from[N];
+
+int n;
+
+
+int main(){
+	int k,a[12];
+	scanf("%d%d",&n,&k);
+	for (int j=0;j<=k;++j) scanf("%d",&a[j]);
+	for (int i=1;i<n;++i){
+		int x=1;
+		for (int j=0;j<=k;++j){
+			cost[i]+=a[j]*x;
+			x=1LL*x*i%mod;
+		}
+		cost[i]%=mod;
+	}
+	dp[0]=cost[1]*n;
+	for (int i=1;i<=n-2;++i){
+		dp[i]=-1;
+		for (int j=2;j<=i+1;++j)
+			if (dp[i]<=dp[i-j+1]-cost[1]+cost[j]){
+				from[i]=i-j+1;
+				dp[i]=dp[i-j+1]-cost[1]+cost[j];
+			}
+	}
+	printf("%d %d\n",n-1,dp[n-2]);
+	int p=n,q=1;
+	for (int i=n-2;i>0;i=from[i],--p){
+		int cnt=i-from[i];
+		if (p<n) printf("%d %d\n",p+1,p),--cnt;
+		while (cnt--) printf("%d %d\n",q++,p);
+	}
+	assert(p==q);
+	printf("%d %d\n",p,p+1);
+	return 0;
+}
