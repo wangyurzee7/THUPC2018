b8bff677e9704889761ad7b73cf74ddc3cbc96a2
wyz <wang_yuzhong@126.com>
2018-04-30 17:24:55 +0800

    remove space@EOLN

--- problems/city/data/maker.cpp
+++ problems/city/data/maker.cpp
@@ -18,8 +18,10 @@ int main()
 		freopen(file,"w",stdout);
 		n=150*((T-1)/5)+rand()%150+1;k=rand()%10+1;
 		printf("%d %d\n",n,k);
-		for (int i=0;i<=k;i++)
-			printf("%d ",(i==k)?(rand()%51):(rand()%50+1));
+		for (int i=0;i<=k;i++){
+			printf("%d",(i==k)?(rand()%51):(rand()%50+1));
+			if (i<k) putchar(' ');
+		}
 		printf("\n");
 	}
 	return 0;
