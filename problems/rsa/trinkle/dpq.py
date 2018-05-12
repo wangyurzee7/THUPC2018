ab9654c9db9ba8e41286f4333bba63e1d073dfa2
n+e <463003665@qq.com>
2018-05-13 00:02:39 +0800

    ...

--- problems/rsa/trinkle/dpq.py
+++ problems/rsa/trinkle/dpq.py
@@ -1,18 +1,14 @@
 import gmpy2
-def work(n,e1,e2,message1,message2):
-        # s & t
-        gcd, s, t = gmpy2.gcdext(e1, e2)
-        if s < 0:
-            s = -s
-            message1 = gmpy2.invert(message1, n)
-        if t < 0:
-            t = -t
-            message2 = gmpy2.invert(message2, n)
-        plain = gmpy2.powmod(message1, s, n) * gmpy2.powmod(message2, t, n) % n
-        print plain
+def work(c1,c2,e1,e2,n):
+    gcd,s,t=gmpy2.gcdext(e1,e2)
+    if s<0:
+        s=-s
+        c1=gmpy2.invert(c1,n)
+    if t<0:
+        t=-t
+        c2=gmpy2.invert(c2,n)
+    return gmpy2.powmod(c1,s,n)*gmpy2.powmod(c2,t,n)%n
 T=int(input())
 for __ in range(T):
         s=raw_input().split(' ')
-        c1,c2,e1,e2,n=int(s[0]),int(s[1]),int(s[2]),int(s[3]),int(s[4])
-        work(n,e1,e2,c1,c2)
-
+        print(work(int(s[0]),int(s[1]),int(s[2]),int(s[3]),int(s[4])))
