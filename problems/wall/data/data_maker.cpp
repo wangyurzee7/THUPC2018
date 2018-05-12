afc60834e5187995b0f3176eea7f0ff88b3553c5
HaHaTa <minehahata@outlook.com>
2018-05-12 20:49:38 +0800

    wall

--- problems/wall/data/data_maker.cpp
+++ problems/wall/data/data_maker.cpp
@@ -50,64 +50,6 @@ void Convex()
 char file[10];
 int num;
 vector<Point>Poly[MAXN];
-void chain(int lim)
-{
-	int num=0,sum=0;
-	vector<Point>Poly[MAXN];
-	Poly[0].clear();
-	while (true)
-	{
-		num++;
-		Poly[num].clear();
-		n=rand()%91+10;
-		for (int i=1;i<=n;i++)
-		{
-			p[i].random();
-			bool flag=1;
-			for (int j=0;j+1<Poly[num-1].size();j++)
-				if (dcmp(Cross(p[i]-Poly[num-1][j],Poly[num-1][j+1]-Poly[num-1][j]))>0)flag=0;
-			if (!flag)i--;
-		}
-		Convex();
-		for (int i=1;i<=m;i++)
-			Poly[num].push_back(ch[i]);
-		if (sum+Poly[num].size()>lim)
-		{
-			num--;
-			break;
-		}
-		sum+=Poly[num].size();
-	}
-	printf("%d 100000\n",num+2);
-	for (int i=1;i<=num;i++)
-	{
-		printf("%d %d ",Poly[i].size(),rand()%2+1);
-		for (int j=0;j<Poly[i].size();j++)
-			printf("%.2lf %.2lf ",Poly[i][j].x,Poly[i][j].y);
-		printf("\n");
-	}
-	printf("4 1 -125000.00 -125000.00 125000.00 -125000.00 125000.00 125000.00 -125000.00 125000.00\n");
-	printf("4 2 -175000.00 -175000.00 175000.00 -175000.00 175000.00 175000.00 -175000.00 175000.00\n");
-	for (int i=1;i<=100000;i++)
-	{
-		int op=rand()%2+1;
-		if (op==1)
-		{
-			printf("%d %d\n",op,rand()%num+1);
-			continue;
-		}
-		Point P;P.random();
-		while (true)
-		{
-			bool flag=1;
-			for (int j=0;j+1<Poly[1].size();j++)
-				if (dcmp(Cross(P-Poly[1][j],Poly[1][j+1]-Poly[1][j]))>0)flag=0;
-			if (flag==1)break;
-			P.random();
-		}
-		printf("%d %d %.2lf %.2lf\n",op,rand()%2+1,P.x,P.y);
-	}
-}
 void reg(int e,double x,double y,double r,int sum,bool flag)
 {
 	if (!flag)num=0;
@@ -129,43 +71,8 @@ void reg(int e,double x,double y,double r,int sum,bool flag)
 			printf("%.2lf %.2lf ",Poly[i][j].x,Poly[i][j].y);
 		printf("\n");
 	}
-	printf("4 1 -125000.00 -125000.00 125000.00 -125000.00 125000.00 125000.00 -125000.00 125000.00\n");
-	printf("4 2 -175000.00 -175000.00 175000.00 -175000.00 175000.00 175000.00 -175000.00 175000.00\n");
-	for (int i=1;i<=100000;i++)
-	{
-		int op=rand()%2+1;
-		if (op==1)
-		{
-			printf("%d %d\n",op,rand()%num+1);
-			continue;
-		}
-		Point P;P.random();
-		while (true)
-		{
-			bool flag=1;
-			for (int j=0;j+1<Poly[1].size();j++)
-				if (dcmp(Cross(P-Poly[1][j],Poly[1][j+1]-Poly[1][j]))>0)flag=0;
-			if (flag==1)break;
-			P.random();
-		}
-		printf("%d %d %.2lf %.2lf\n",op,rand()%2+1,P.x,P.y);
-	}
-}
-void multi_reg(int n)
-{
-	num=0;
-	for (int i=1;i<=n;i++)
-		reg(rand()%91+10,0,double(i-1)*100000/double(n),double(i-1)*100000/double(n),100000/n,1);
-	printf("%d 100000\n",num+2);
-	for (int i=1;i<=num;i++)
-	{
-		printf("%d %d ",Poly[i].size(),rand()%2+1);
-		for (int j=0;j<Poly[i].size();j++)
-			printf("%.2lf %.2lf ",Poly[i][j].x,Poly[i][j].y);
-		printf("\n");
-	}
-	printf("4 1 -125000.00 -125000.00 125000.00 -125000.00 125000.00 125000.00 -125000.00 125000.00\n");
-	printf("4 2 -175000.00 -175000.00 175000.00 -175000.00 175000.00 175000.00 -175000.00 175000.00\n");
+	printf("4 1 -198000.00 -198000.00 198000.00 -198000.00 198000.00 198000.00 -198000.00 198000.00\n");
+	printf("4 2 -199000.00 -199000.00 199000.00 -199000.00 199000.00 199000.00 -199000.00 199000.00\n");
 	for (int i=1;i<=100000;i++)
 	{
 		int op=rand()%2+1;
@@ -189,9 +96,22 @@ void multi_reg(int n)
 void mum(int e)
 {
 	num=0;
-	reg(e,0,0,100000,e,1);
-	for (int i=-50000;i<=50000;i+=10)
-		reg(rand()%7+3,i,i,9,9,1);
+	reg(e,0,0,197500,e,1);
+	for (int i=-100000;i<=100000;i+=10)
+	{
+		int d=rand()%8+3;
+		reg(d,i,i,10,d,1);
+	}
+	int sum=0;
+	for (int i=1;i<=num;i++)
+	{
+		sum+=Poly[i].size();
+		if (sum>99992)
+		{
+			num=i;
+			break;
+		}
+	}
 	printf("%d 100000\n",num+2);
 	for (int i=1;i<=num;i++)
 	{
@@ -200,8 +120,8 @@ void mum(int e)
 			printf("%.2lf %.2lf ",Poly[i][j].x,Poly[i][j].y);
 		printf("\n");
 	}
-	printf("4 1 -125000.00 -125000.00 125000.00 -125000.00 125000.00 125000.00 -125000.00 125000.00\n");
-	printf("4 2 -175000.00 -175000.00 175000.00 -175000.00 175000.00 175000.00 -175000.00 175000.00\n");
+	printf("4 1 -198000.00 -198000.00 198000.00 -198000.00 198000.00 198000.00 -198000.00 198000.00\n");
+	printf("4 2 -199000.00 -199000.00 199000.00 -199000.00 199000.00 199000.00 -199000.00 199000.00\n");
 	for (int i=1;i<=100000;i++)
 	{
 		int op=rand()%2+1;
@@ -226,20 +146,19 @@ int main()
 {
 	srand(time(NULL));
 	for (int T=1;T<=10;T++)
-	{ 
-		if (T<8)continue; 
+	{
 		sprintf(file,"%d.in",T);
 		freopen(file,"w",stdout);
 		switch(T)
 		{
-			case 1:chain(50);break;
-			case 2:chain(50);break;
-			case 3:chain(50);break;
-			case 4:reg(97,0,0,100000,100000,0);break;
-			case 5:reg(93,0,0,100000,100000,0);break;
-			case 6:multi_reg(4);break;
-			case 7:multi_reg(8);break;
-			case 8:mum(98);break;
+			case 1:reg(3,0,0,197500,100000,0);break;
+			case 2:reg(7,0,0,197500,100000,0);break;
+			case 3:reg(23,0,0,197500,100000,0);break;
+			case 4:reg(67,0,0,197500,100000,0);break;
+			case 5:reg(97,0,0,197500,100000,0);break;
+			case 6:mum(69);break;
+			case 7:mum(79);break;
+			case 8:mum(89);break;
 			case 9:mum(99);break;
 			case 10:mum(100);break;
 			default:break;
--- problems/wall/data/run.bat
+++ problems/wall/data/run.bat
@@ -5,3 +5,6 @@ ans_maker.exe <4.in >4.ans
 ans_maker.exe <5.in >5.ans
 ans_maker.exe <6.in >6.ans
 ans_maker.exe <7.in >7.ans
+ans_maker.exe <8.in >8.ans
+ans_maker.exe <9.in >9.ans
+ans_maker.exe <10.in >10.ans
